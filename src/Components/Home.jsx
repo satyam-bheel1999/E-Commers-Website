1a3ee40aa7025b0e704cb27c1faeca2774fa223f
Satyam Bheel <136482276+satyam-bheel1999@users.noreply.github.com>
2025-02-03 16:04:34 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,24 +9,26 @@ function Home() {
 
     const [cart, setCart] = useState([]);
 
-    const handleCart = (productId) =>{
+    const [filteredProducts, setFilteredProducts] = useState([]);
 
-        
+    const handleCart = (productId) => {
+
+
 
     }
 
 
-    useEffect( () =>{
-        const fetchData = async() =>{
+    useEffect(() => {
+        const fetchData = async () => {
 
-            try{
+            try {
                 let productResponse = await axios.get('https://fakestoreapi.com/products');
                 setProducts(productResponse.data);
 
                 console.log(products);
 
-            } catch(erro){
-                    console.log("error")
+            } catch (erro) {
+                console.log("error")
             }
 
         }
@@ -35,49 +37,86 @@ function Home() {
     }, [])
 
 
-
-  return (
-    <div>
-
-        <h1>Products</h1>
-
-        {/*Search bar*/}
-        <div className='w-screen h-20 flex justify-center items-center' >
-        <input type='text' value={search} onChange={(e) => setSearch(e.target.value) } placeholder='Search Product' className=' border-b-2 p-2 mr-5 focus:outline-none'></input>
-
-        <button className='border flex flex-row p-2 cursor-pointer rounded-2xl hover:bg-emerald-600'>Search <img className='h-6 ml-3' src='src\Components\Images\magnifying-glass-solid.svg' /></button>
-
-        <button className='border flex flex-row p-2 cursor-pointer absolute right-3'>Filter <img className='h-6 ml-3' src='src\Components\Images\filter-solid.svg' /> </button>
-
-        </div>
-        <ul className='grid grid-cols-5 gap-3'>
-            {
-                products.map( product => (
-    
-                    <li key={product.id} className='h-72 w-96 flex flex-col justify-center items-center cursor-pointer
-                    transition-transform duration-100 ease-in-out hover:translate-y-2'>
-                        <img src={product.image} alt={product.title} className='w-32 h-32' />
-                        <div className='flex flex-col justify-center items-center'>
-
-                            <h3>{product.title}</h3>
-                            <p>${product.price}</p>
+    const searchedProduct = () => {
+        const filtered = products.filter((product) =>
+            product.title.toLowerCase().includes(search.toLowerCase())
+        );
+        setFilteredProducts(filtered);
+    };
 
 
-                        </div>
-                        <button className='h-11 w-24 border border-emerald-600 font-semibold hover:bg-emerald-600 cursor-pointer' onClick={handleCart(product.id)}>Add to Cart</button>
-                            
-                    </li>
-                ))
-            }
-        </ul>
+
+    return (
         <div>
 
-        </div>
+            <h1>Products</h1>
 
-        
-      
-    </div>
-  )
+            {/*Search bar*/}
+            <div className='w-screen h-20 flex justify-center items-center' >
+                <input type='text' value={search} onChange={(e) => setSearch(e.target.value)} placeholder='Search Product' className=' border-b-2 p-2 mr-5 focus:outline-none'></input>
+
+                <button onClick={searchedProduct} className='border flex flex-row p-2 cursor-pointer rounded-2xl hover:bg-emerald-600'>Search <img className='h-6 ml-3' src='src\Components\Images\magnifying-glass-solid.svg' /></button>
+
+                <button className='border flex flex-row p-2 cursor-pointer absolute right-3'>Filter <img className='h-6 ml-3' src='src\Components\Images\filter-solid.svg' /> </button>
+
+            </div>
+            <div>
+                <ul className='grid grid-cols-5 gap-3'>
+
+                    {
+                        filteredProducts.length > 0 ? (
+                            filteredProducts.map((product) => (
+                                <li key={product.id} className='h-72 w-96 flex flex-col justify-center items-center cursor-pointer
+                    transition-transform duration-100 ease-in-out hover:translate-y-2'>
+                                    <img src={product.image} alt={product.title} className='w-32 h-32' />
+                                    <div className='flex flex-col justify-center items-center'>
+
+                                        <h3>{product.title}</h3>
+                                        <p>${product.price}</p>
+
+
+                                    </div>
+                                    <button className='h-11 w-24 border border-emerald-600 font-semibold hover:bg-emerald-600 cursor-pointer' onClick={handleCart(product.id)}>Add to Cart</button>
+
+                                </li>
+                            ))
+                        ) : (
+
+                            products.map(product => (
+
+                                <li key={product.id} className='h-72 w-96 flex flex-col justify-center items-center cursor-pointer
+                            transition-transform duration-100 ease-in-out hover:translate-y-2'>
+                                    <img src={product.image} alt={product.title} className='w-32 h-32' />
+                                    <div className='flex flex-col justify-center items-center'>
+
+                                        <h3>{product.title}</h3>
+                                        <p>${product.price}</p>
+
+
+                                    </div>
+                                    <button className='h-11 w-24 border border-emerald-600 font-semibold hover:bg-emerald-600 cursor-pointer' onClick={handleCart(product.id)}>Add to Cart</button>
+
+                                </li>
+                            ))
+
+                        )
+                    }
+
+
+
+
+                </ul>
+
+            </div>
+
+            <div>
+
+            </div>
+
+
+
+        </div>
+    )
 }
 
 export default Home
